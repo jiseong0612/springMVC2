e28b7ae202405f7fbadc75683b05c5e4a556abb2
jiseong0612 <hth5545@gmail.com>
2022-03-11 10:14:24 +0900

    first-commit

---
+++ src/main/java/hello/thmeleaf/ThmeleafBasicApplication.java
@@ -0,0 +1,13 @@
+package hello.thmeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThmeleafBasicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThmeleafBasicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hello/thmeleaf/ThmeleafBasicApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.thmeleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThmeleafBasicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
