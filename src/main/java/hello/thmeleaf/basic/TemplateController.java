b0b5fa54268cfb129d205d4e4efa0d11624ccb45
jiseong0612 <hth5545@gmail.com>
2022-03-13 10:41:00 +0900

    first-commit

--- src/main/java/hello/thmeleaf/basic/TemplateController.java
+++ src/main/java/hello/thmeleaf/basic/TemplateController.java
@@ -12,4 +12,9 @@ public class TemplateController {
 		return "template/fragment/fragmentMain";
 	}
 	
+	@GetMapping("/layout")
+	public String layout() {
+		return "template/layout/layoutMain";
+	}
+	
 }
